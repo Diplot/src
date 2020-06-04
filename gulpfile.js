a9cd8c51554237555b111736018ec04fdb4e7fcc
Platonov Dmitry <dmytro.platonov.it@gmail.com>
2020-06-04 23:32:57 +0300

    дз от 03.06

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var pipeline = require('readable-stream').pipeline;
  
-gulp.task('compress', function () {
+gulp.task('js', function () {
   return pipeline(
         gulp.src('src/js/app.js'),
         uglify(),
@@ -14,4 +14,10 @@ gulp.task('scss', function(){
     return gulp.src('src/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
+});
+gulp.task('default', gulp.series(['scss','js'],));
+
+gulp.task('watch', function(){
+    gulp.watch( 'src/**/*.scss', gulp.series(['scss']));
+    gulp.watch( 'src/**/*.js', gulp.series(['js'])) 
 });
